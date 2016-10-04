b93dd50a91826f857be1249ffcfc6d4dcef66f8b
Conrad Beach <conradbeach@gmail.com>
2016-10-04 09:27:47 -0600

    Add Bower, Grunt and eslint config.

---
+++ gruntfile.js
@@ -0,0 +1,67 @@
+function extractFilename(filePath) {
+  return filePath.split('/').pop().match(/(.+)\.hbs/).pop();
+}
+
+module.exports = function Grunt(grunt) {
+  var sources = ['public/javascripts/vendor/all.js',
+                 'public/javascripts/models/*.js',
+                 'public/javascripts/collections/*.js',
+                 'public/javascripts/views/*.js',
+                 'public/javascripts/app.js'];
+
+  grunt.initConfig({
+    bower_concat: {
+      all: {
+        dest: {
+          js: 'public/javascripts/vendor/all.js'
+        },
+        dependencies: {
+          underscore: 'jquery',
+          backbone: 'underscore'
+        }
+      }
+    },
+
+    concat: {
+      basic: {
+        src: sources,
+        dest: 'public/javascripts/app.full.js'
+      }
+    },
+
+    handlebars: {
+      all: {
+        files: {
+          'public/javascripts/handlebars_templates.js': 'handlebars/**/*.hbs'
+        },
+        options: {
+          processName: extractFilename
+        }
+      }
+    },
+
+    uglify: {
+      my_target: {
+        files: {
+          'public/javascripts/app.min.js': sources
+        }
+      }
+    },
+
+    watch: {
+      files: ['public/javascripts/**/*.js',
+              '!public/javascripts/app.min.js',
+              '!public/javascripts/app.full.js',
+              'handlebars/*.hbs'],
+      tasks: ['concat', 'handlebars']
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-bower-concat');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-handlebars');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  grunt.registerTask('default', ['bower_concat', 'concat', 'handlebars']);
+};
