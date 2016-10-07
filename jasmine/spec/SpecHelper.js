f92d8d25434b32d92a7dd9b14e1b2a593d248beb
Conrad Beach <conradbeach@gmail.com>
2016-10-07 08:36:40 -0600

    Add TodosView spec.

--- jasmine/spec/SpecHelper.js
+++ jasmine/spec/SpecHelper.js
@@ -1,7 +1,7 @@
 beforeEach(function () {
   this.seedCollection = function() {
     app.todos.reset();
-    app.todos.create({ completed: true, dueDate: new Date('10-20-2016') });
-    app.todos.create({ completed: false, dueDate: new Date('5-20-2016') });
+    app.todos.create({ title: 'Task 1', completed: true, dueDate: new Date('10-20-2016') });
+    app.todos.create({ title: 'Task 2', completed: false, dueDate: new Date('5-20-2016') });
   };
 });
---
+++ jasmine/spec/TodosViewSpec.js
@@ -0,0 +1,29 @@
+describe('TodosView view', function() {
+  beforeEach(function() {
+    this.seedCollection();
+
+    setFixtures('<main><ul></ul></main>');
+
+    this.view = new TodosView({ el: 'main ul' });
+  });
+
+  it('renders todos based on current filter', function() {
+    app.todoFilter = 'all-false';
+    this.view.render();
+    expect(this.view.$('li').length).toEqual(1);
+    expect(this.view.$('li').html()).toContain('Task 2');
+    expect(this.view.$('li').html()).toContain('May 20, 2016');
+
+    app.todoFilter = 'all-true';
+    this.view.render();
+    expect(this.view.$('li').length).toEqual(1);
+    expect(this.view.$('li').html()).toContain('Task 1');
+    expect(this.view.$('li').html()).toContain('October 20, 2016');
+
+    app.todoFilter = '10/2016-true';
+    this.view.render();
+    expect(this.view.$('li').length).toEqual(1);
+    expect(this.view.$('li').html()).toContain('Task 1');
+    expect(this.view.$('li').html()).toContain('October 20, 2016');
+  });
+});
